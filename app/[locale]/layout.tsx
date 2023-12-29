b0216a8cfe1f7adfaa1cb09ad2f9a986fae99917
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2023-12-29 21:17:03 +0100

    updates

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -129,7 +129,6 @@ export default function RootLayout({
           GeistSans.variable,
           GeistMono.variable
         )}
-        suppressHydrationWarning
       >
         <Toaster />
         <Providers
