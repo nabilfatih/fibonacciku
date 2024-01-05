e47c728c13586932c24dbbb4a15f8798ad612bc1
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-01-05 13:03:19 +0100

    updates

---
+++ app/[locale]/(application)/share/[feature]/[id]/page.tsx
@@ -0,0 +1,56 @@
+import ChatMessage from "@/components/chat";
+import supabaseAdmin from "@/lib/supabase/admin";
+import { kv } from "@vercel/kv";
+import type { Metadata } from "next";
+import { notFound } from "next/navigation";
+
+type Props = {
+  params: { feature: string; id: string };
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const chatId = params.id;
+
+  const title = await kv.get<string>(chatId).then(title => {
+    if (title) {
+      return title.toString().slice(0, 50);
+    }
+  });
+
+  if (!title)
+    return {
+      title: {
+        absolute: "FibonacciKu",
+      },
+    };
+
+  return {
+    title: title,
+  };
+}
+
+export default async function ChatMessagePage({ params }: Props) {
+  // can be open without login
+  const { data: chat } = await supabaseAdmin
+    .from("chat")
+    .select()
+    .eq("id", params.id)
+    .limit(1)
+    .maybeSingle();
+
+  if (!chat) {
+    notFound();
+  }
+  return (
+    <ChatMessage
+      id={chat.id}
+      userId={chat.user_id}
+      initialMessages={chat.messages}
+      type={chat.type as "assistant" | "document"}
+      title={chat.title}
+      fileId={chat.file_id}
+      createdAt={chat.created_at}
+      chat={chat}
+    />
+  );
+}
---
+++ app/[locale]/(application)/share/layout.tsx
@@ -0,0 +1,18 @@
+import { MessageContextProvider } from "@/lib/context/use-message";
+import { Suspense } from "react";
+
+export default function ChatLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="relative flex h-[calc(100dvh-4rem)] overflow-hidden">
+      <div className="group w-full overflow-hidden pl-0 duration-300 ease-in-out animate-in">
+        <MessageContextProvider>
+          <Suspense>{children}</Suspense>
+        </MessageContextProvider>
+      </div>
+    </div>
+  );
+}
