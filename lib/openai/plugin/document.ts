5bb8fb53ea39d297ec2904b770a39f4189708b02
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-01-07 00:00:57 +0100

    updates

--- lib/openai/plugin/document.ts
+++ lib/openai/plugin/document.ts
@@ -1,6 +1,10 @@
 import supabaseAdmin from "@/lib/supabase/admin";
 import { OpenAIEmbeddings } from "@langchain/openai";
 import { SupabaseHybridSearch } from "@langchain/community/retrievers/supabase";
+import {
+  type SupabaseFilterRPCCall,
+  SupabaseVectorStore,
+} from "@langchain/community/vectorstores/supabase";
 
 export const documentRetrieval = async (
   userId: string,
@@ -12,20 +16,41 @@ export const documentRetrieval = async (
     openAIApiKey: process.env.OPENAI_API_KEY,
   });
 
+  // const filter: SupabaseFilterRPCCall = rpc =>
+  //   rpc
+  //     .filter("metadata->>file_id", "eq", fileId)
+  //     .filter("metadata->>user_id", "eq", userId);
+
+  // const store = await SupabaseVectorStore.fromExistingIndex(embeddings, {
+  //   client: supabaseAdmin,
+  //   tableName: "documents",
+  //   queryName: "match_documents",
+  // });
+
+  const metadata = {
+    file_id: fileId,
+    user_id: userId,
+  };
+
   const retriever = new SupabaseHybridSearch(embeddings, {
     client: supabaseAdmin,
-    similarityK: 10,
-    keywordK: 10,
+    similarityK: 20,
+    keywordK: 20,
     tableName: "documents",
     similarityQueryName: "match_documents",
     keywordQueryName: "kw_match_documents",
-    metadata: {
-      file_id: fileId,
-      user_id: userId,
-    },
+    metadata,
   });
 
-  const results = await retriever.getRelevantDocuments(query);
+  // const results = await store.maxMarginalRelevanceSearch(query, {
+  //   k: 10,
+  //   fetchK: 10,
+  //   filter,
+  // });
+
+  const results = await retriever.getRelevantDocuments(query, {
+    metadata,
+  });
 
   const cleanData = results
     .slice(0, 7)
@@ -39,7 +64,9 @@ export const documentRetrieval = async (
     .filter(
       (v, i, a) =>
         a.findIndex(t => t.metadata.page_number === v.metadata.page_number) ===
-        i
+          i && v.metadata.file_id === fileId // Idk why the supabase return other documents, so I filter it here
+      // this is only temporary solution
+      // TODO: Maybe there is a better way to do this
     )
     // oder by page number
     .sort((a, b) => a.page_number - b.page_number);
