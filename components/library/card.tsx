b3d60e75dcc97eaae6ce7d51763d9224a0175554
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-01-07 20:01:36 +0100

    updates

--- components/library/card.tsx
+++ components/library/card.tsx
@@ -13,17 +13,17 @@ export default function LibraryCard({ library }: Props) {
     <motion.div
       layout // This prop indicates that the component is part of a shared layout animation
       key={library.id}
-      variants={{
-        hidden: { opacity: 0 },
-        visible: { opacity: 1 },
-      }}
       initial={{ opacity: 0, x: 30 }}
       animate={{
         opacity: 1,
         x: 0,
-        transition: { duration: 0.15, ease: "easeInOut" },
+        transition: { duration: 0.25, ease: "easeInOut" },
       }}
       exit={{ opacity: 0, x: 20, transition: { duration: 0.3 } }}
+      transition={{
+        duration: 0.25,
+        ease: "easeInOut",
+      }}
       className="mb-2 rounded-xl border px-4 py-3 shadow-sm"
     >
       <div className="flex w-full flex-col gap-2 sm:flex-row sm:items-center">
