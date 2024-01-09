b53b735a50c39a68242c08eb4190a74119a9396f
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-01-09 16:21:57 +0100

    updates

--- app/[locale]/(application)/(main)/account/page.tsx
+++ app/[locale]/(application)/(main)/account/page.tsx
@@ -21,7 +21,7 @@ export default async function AccountPage() {
     <>
       <AccountHeader text="account" />
 
-      <main className="h-[calc(100%-81px)] space-y-4 overflow-y-auto overflow-x-hidden pb-4 pt-4">
+      <main className="h-[calc(100%-81px)] space-y-4 overflow-y-auto overflow-x-hidden py-6">
         <Suspense>
           <AccountGeneral userId={session.user.id} />
         </Suspense>
