ca78ec162db22a27a9c1253311fe668b298e0bbe
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-01-11 11:52:40 +0100

    updates

--- components/chat/panel.tsx
+++ components/chat/panel.tsx
@@ -47,7 +47,7 @@ export default function ChatPanel({
   const [shareDialogOpen, setShareDialogOpen] = React.useState(false);
 
   return (
-    <div className="fixed inset-x-0 bottom-0 w-full bg-gradient-to-b from-transparent via-muted/50 to-muted duration-300 ease-in-out animate-in peer-[[data-state=open]]:group-[]:lg:pl-[250px] peer-[[data-state=open]]:group-[]:xl:pl-[300px] dark:from-transparent dark:via-background/50 dark:to-background">
+    <div className="fixed inset-x-0 bottom-0 w-full bg-gradient-to-b from-transparent via-muted/50 to-muted duration-300 ease-in-out animate-in dark:from-transparent dark:via-background/50 dark:to-background peer-[[data-state=open]]:group-[]:lg:pl-[250px] peer-[[data-state=open]]:group-[]:xl:pl-[300px]">
       <SidebarDocument>
         <ChatDocument />
       </SidebarDocument>
--- components/library/panel.tsx
+++ components/library/panel.tsx
@@ -3,7 +3,7 @@ import LibraryForm from "@/components/library/form";
 
 export default function LibraryPanel() {
   return (
-    <div className="fixed inset-x-0 bottom-0 w-full bg-gradient-to-b from-transparent via-muted/50 to-muted duration-300 ease-in-out animate-in peer-[[data-state=open]]:group-[]:lg:pl-[250px] peer-[[data-state=open]]:group-[]:xl:pl-[300px] dark:from-transparent dark:via-background/50 dark:to-background">
+    <div className="fixed inset-x-0 bottom-0 w-full bg-gradient-to-b from-transparent via-muted/50 to-muted duration-300 ease-in-out animate-in dark:from-transparent dark:via-background/50 dark:to-background peer-[[data-state=open]]:group-[]:lg:pl-[250px] peer-[[data-state=open]]:group-[]:xl:pl-[300px]">
       <div className="mx-auto sm:max-w-2xl sm:px-2">
         <div className="space-y-2 border-t bg-background p-2 sm:border-none sm:bg-transparent">
           <LibraryForm />
