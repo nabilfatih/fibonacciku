6c5115da64b7cd927c629a22b33fa2ec3c4a4c59
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-01-11 14:48:07 +0100

    updates

--- components/premium/features.tsx
+++ components/premium/features.tsx
@@ -2,6 +2,8 @@ import { getScopedI18n } from "@/locales/server";
 import {
   IconBook2,
   IconFiles,
+  IconPhotoHeart,
+  IconPhotoScan,
   IconPuzzle,
   IconSparkles,
 } from "@tabler/icons-react";
@@ -10,7 +12,7 @@ export default async function PremiumFeatures() {
   const t = await getScopedI18n("MarketingPricing");
   return (
     <section className="mx-auto max-w-4xl px-4">
-      <div className="grid grid-cols-1 gap-4 py-6 sm:grid-cols-2 sm:grid-rows-2 sm:gap-y-6">
+      <div className="grid grid-cols-1 gap-4 py-6 sm:grid-cols-2 sm:grid-rows-3 sm:gap-y-6">
         <div className="flex items-start gap-2">
           <IconSparkles className="h-6 w-6 min-w-6" />
           <div className="flex flex-col gap-1">
@@ -18,8 +20,8 @@ export default async function PremiumFeatures() {
               {t("you-can-do-anything")}
             </h2>
             <p className="text-sm text-muted-foreground">
-              Unlimited access to all features no matter how much you use our
-              service. No limit, no restriction.
+              Unlimited access to all features no matter how much you use fibo.
+              No limit, no restriction.
             </p>
           </div>
         </div>
@@ -31,8 +33,8 @@ export default async function PremiumFeatures() {
               Easy upload documents
             </h2>
             <p className="text-sm text-muted-foreground">
-              Effortless upload documents to our service. Just drag and drop.
-              Again, no limit.
+              Effortless upload documents to fibo. Just drag and drop. Again, no
+              limit.
             </p>
           </div>
         </div>
@@ -59,6 +61,32 @@ export default async function PremiumFeatures() {
             <p className="text-sm text-muted-foreground">
               Explore our books collection. Read it online, download, or chat
               with it.
+            </p>
+          </div>
+        </div>
+
+        <div className="flex items-start gap-2">
+          <IconPhotoScan className="h-6 w-6 min-w-6" />
+          <div className="flex flex-col gap-1">
+            <h2 className="text-lg font-semibold leading-none tracking-tight">
+              Image analysis
+            </h2>
+            <p className="text-sm text-muted-foreground">
+              Upload your image and ask anything about it. Full integration with
+              the fibo.
+            </p>
+          </div>
+        </div>
+
+        <div className="flex items-start gap-2">
+          <IconPhotoHeart className="h-6 w-6 min-w-6" />
+          <div className="flex flex-col gap-1">
+            <h2 className="text-lg font-semibold leading-none tracking-tight">
+              Limitless image generation
+            </h2>
+            <p className="text-sm text-muted-foreground">
+              Let us know your imagination and fibo will generate it for you.
+              Powered by DALL-E AI model.
             </p>
           </div>
         </div>
