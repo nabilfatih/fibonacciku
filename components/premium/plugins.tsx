6b28cd5a4db64aa71a8b369bb12acc0fed300d83
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-01-11 16:42:39 +0100

    updates

--- components/premium/plugins.tsx
+++ components/premium/plugins.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 
-export default async function PremiumPlugins() {
+export default function PremiumPlugins() {
   return (
     <section className="mx-auto max-w-4xl px-4">
       <div className="flex justify-center">
