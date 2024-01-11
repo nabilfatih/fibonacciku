25c5cedff766def42b545fa971661dd833ef6043
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-01-11 16:42:24 +0100

    updates

--- components/premium/plugins.tsx
+++ components/premium/plugins.tsx
@@ -3,7 +3,11 @@ import { Button } from "@/components/ui/button";
 export default async function PremiumPlugins() {
   return (
     <section className="mx-auto max-w-4xl px-4">
-      <Button variant="outline">See our plugins</Button>
+      <div className="flex justify-center">
+        <Button variant="outline" className="w-full sm:w-auto">
+          See our plugins
+        </Button>
+      </div>
     </section>
   );
 }
