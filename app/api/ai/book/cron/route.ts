2ac2e467d3dc3eb1e4c6bbbd4d7fb5bf3273b3cd
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-01-12 20:49:45 +0100

    updates

--- app/api/ai/book/cron/route.ts
+++ app/api/ai/book/cron/route.ts
@@ -86,6 +86,7 @@ export async function GET(req: Request) {
         const metadata = {
           book_id: book.id,
           file_id: book.file_id,
+          num_pages: cleanData.length,
         };
 
         const splitDocs = await createDocumentsFromPages(
--- app/api/script/book/route.ts
+++ app/api/script/book/route.ts
@@ -82,6 +82,7 @@ export async function POST(req: Request) {
       const metadata = {
         book_id: book.id,
         file_id: book.file_id,
+        num_pages: cleanData.length,
       };
 
       const splitDocs = await createDocumentsFromPages(
