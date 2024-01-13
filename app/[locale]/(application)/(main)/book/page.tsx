f2b653ecb0673168dda7fb05332bc218e9dcff1c
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-01-13 12:52:54 +0100

    updates

--- app/[locale]/(application)/(main)/book/page.tsx
+++ app/[locale]/(application)/(main)/book/page.tsx
@@ -1,8 +1,31 @@
 import { createClientServer } from "@/lib/supabase/server";
+import { getScopedI18n } from "@/locales/server";
+import type { Metadata } from "next";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
+export async function generateMetadata(): Promise<Metadata> {
+  const t = await getScopedI18n("ProductFiboBook");
+
+  return {
+    title: {
+      absolute: t("fibo-book"),
+    },
+    description: `${t("header")}. ${t("fibo-book-description")}`,
+    alternates: {
+      canonical: "/book",
+      languages: {
+        en: "/en/book",
+        id: "/id/book",
+        ms: "/de/book",
+      },
+    },
+  };
+}
+
 export default async function BookPage() {
+  const t = await getScopedI18n("Book");
+
   const cookieStore = cookies();
   const supabase = createClientServer(cookieStore);
   const {
