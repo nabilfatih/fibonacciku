f1834f71de8f10fe23f311c4953fc11c2e11fd07
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-01-15 18:02:11 +0100

    updates

--- app/[locale]/(application)/(main)/chat/[feature]/page.tsx
+++ app/[locale]/(application)/(main)/chat/[feature]/page.tsx
@@ -1,21 +1,46 @@
-import { createClientServer } from "@/lib/supabase/server";
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
-import ChatMessage from "@/components/chat";
+import { createClientServer } from "@/lib/supabase/server"
+import { cookies } from "next/headers"
+import { redirect } from "next/navigation"
+import ChatMessage from "@/components/chat"
+import type { Metadata } from "next"
+import { getScopedI18n } from "@/locales/server"
 
 type Props = {
-  params: { feature: string };
-};
+  params: { feature: string }
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const t = await getScopedI18n("MarketingProduct")
+  const feature = params.feature
+
+  return {
+    title: {
+      absolute: t(`fibo-${feature}` as never)
+    },
+    description:
+      feature === "assistant"
+        ? t("fibo-assistant-assistant")
+        : t("fibo-assistant-document"),
+    alternates: {
+      canonical: `/chat/${feature}`,
+      languages: {
+        en: `/en/chat/${feature}`,
+        id: `/id/chat/${feature}`,
+        de: `/de/chat/${feature}`
+      }
+    }
+  }
+}
 
 export default async function ChatFeaturePage({ params }: Props) {
-  const cookieStore = cookies();
-  const supabase = createClientServer(cookieStore);
+  const cookieStore = cookies()
+  const supabase = createClientServer(cookieStore)
   const {
-    data: { session },
-  } = await supabase.auth.getSession();
+    data: { session }
+  } = await supabase.auth.getSession()
 
   if (!session?.user) {
-    redirect(`/auth/login?next=/chat/${params.feature}`);
+    redirect(`/auth/login?next=/chat/${params.feature}`)
   }
 
   return (
@@ -23,5 +48,5 @@ export default async function ChatFeaturePage({ params }: Props) {
       userId={session.user.id}
       type={params.feature as "assistant" | "document"}
     />
-  );
+  )
 }
