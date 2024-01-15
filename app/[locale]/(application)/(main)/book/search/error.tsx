ab41336008a1563641db9c6b408669f481b3d303
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-01-15 10:41:13 +0100

    updates

---
+++ app/[locale]/(application)/(main)/book/search/error.tsx
@@ -0,0 +1,33 @@
+"use client" // Error components must be Client Components
+
+import { Button } from "@/components/ui/button"
+import { useScopedI18n } from "@/locales/client"
+import Link from "next/link"
+
+export default function Error({
+  error,
+  reset
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  const t = useScopedI18n("BackendRouter")
+
+  return (
+    <main className="mx-auto max-w-2xl px-4">
+      <h1 className="mb-2 text-xl font-semibold sm:text-3xl">
+        {t("high-traffic")}
+      </h1>
+
+      <div>
+        <p className="mb-6 leading-normal text-muted-foreground">
+          {t("something-wrong")}
+        </p>
+
+        <Button asChild>
+          <Link href="/book">{t("try-again")}</Link>
+        </Button>
+      </div>
+    </main>
+  )
+}
