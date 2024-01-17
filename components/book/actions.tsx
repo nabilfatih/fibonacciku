784193ebea8f69bc055f5dd2385c9565d2406419
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-01-17 21:07:20 +0100

    updates

--- components/book/actions.tsx
+++ components/book/actions.tsx
@@ -1,7 +1,14 @@
+import { Button } from "@/components/ui/button"
+
 export default function BookActions() {
   return (
     <div className="fixed inset-x-0 bottom-0 h-16 w-full border-t duration-300 ease-in-out animate-in peer-[[data-state=open]]:group-[]:lg:pl-[250px] peer-[[data-state=open]]:group-[]:xl:pl-[300px]">
-      <div className="mx-auto w-full max-w-2xl px-4"></div>
+      <div className="mx-auto w-full max-w-2xl px-4 flex items-center justify-between">
+        <div></div>
+        <div className="flex-1">
+          <div className="flex items-center"></div>
+        </div>
+      </div>
     </div>
   )
 }
