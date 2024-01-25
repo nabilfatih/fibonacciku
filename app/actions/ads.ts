d2f51297c2969abcd894b89be2bed5a4a405c2e7
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-01-25 12:49:13 +0100

    updates

--- app/actions/ads.ts
+++ app/actions/ads.ts
@@ -2,7 +2,7 @@
 
 export const getAdsAdzedek = async () => {
   const response = await fetch(
-    `https://api.adzedek.com/fetch_ad_by_id/adzedekTest`,
+    `https://api.adzedek.com/get_ad_info_by_id/adzedekTest`,
     {
       headers: {
         "adzedek-api": process.env.ADZEDEK_API_KEY as string
@@ -23,4 +23,27 @@ export const getAdsAdzedek = async () => {
   }
 }
 
-export const clickAdsAdzedek = async (adId: string, userId: string) => {}
+export const clickAdsAdzedek = async (adId: string, userId: string) => {
+  const response = await fetch("https://api.adzedek.com/record_user_click", {
+    method: "POST",
+    body: JSON.stringify({
+      ad_id: adId,
+      user_id: userId
+    }),
+    headers: {
+      "adzedek-api": process.env.ADZEDEK_API_KEY as string
+    }
+  })
+
+  if (!response.ok) {
+    return {
+      error: "Something went wrong"
+    }
+  }
+
+  const data = await response.json()
+
+  return {
+    data
+  }
+}
--- components/ads/box.tsx
+++ components/ads/box.tsx
@@ -3,14 +3,22 @@
 import Link from "next/link"
 import useSWR from "swr"
 
+import { useCurrentUser } from "@/lib/context/use-current-user"
+import { generateUUID } from "@/lib/utils"
+
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
-import { getAdsAdzedek } from "@/app/actions/ads"
+import { clickAdsAdzedek, getAdsAdzedek } from "@/app/actions/ads"
 
 export default function AdsBox() {
   const { data, isLoading } = useSWR("ads", getAdsAdzedek)
+  const {
+    userDetails,
+    subscription,
+    isLoading: isUserLoading
+  } = useCurrentUser()
 
-  if (isLoading || !data) {
+  if (isLoading || isUserLoading || !data || subscription || !userDetails) {
     return null
   }
 
@@ -18,11 +26,17 @@ export default function AdsBox() {
 
   const ads = data.data
 
+  const handleClick = () => {
+    clickAdsAdzedek(ads.id, generateUUID())
+    // add new tab
+    window.open(ads.link, "_blank")
+  }
+
   return (
     <section className="mx-auto max-w-xs p-6">
-      <Card>
+      <Card className="cursor-pointer" onClick={handleClick}>
         <CardHeader className="pb-3 pt-5">
-          <CardTitle>SPONSORED</CardTitle>
+          <CardTitle className="text-foreground/80">SPONSORED</CardTitle>
         </CardHeader>
         <CardContent className="pb-3">
           <p className="text-muted-foreground">{ads.text}</p>
