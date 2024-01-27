c0f5887e2d555a84adf9a157c6a0da4f8409842f
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-01-27 22:00:26 +0100

    updates

--- components/emails/welcome.tsx
+++ components/emails/welcome.tsx
@@ -46,7 +46,7 @@ export default function WelcomeEmail({
       </Head>
       <Preview>{previewText}</Preview>
       <Tailwind>
-        <Body className="bg-neutral mx-auto my-auto font-sans text-zinc-100">
+        <Body className="bg-zinc-100 mx-auto my-auto font-sans text-zinc-900">
           <Container className="mx-auto my-10 w-[465px] p-5">
             <Section className="mt-8">
               <Img
