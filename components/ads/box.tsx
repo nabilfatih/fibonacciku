5c8681336a1e6a6e675c8f659bf600ee98ca412a
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-02-01 00:42:22 +0100

    updates

--- components/ads/box.tsx
+++ components/ads/box.tsx
@@ -33,7 +33,7 @@ export default function AdsBox({ id }: Props) {
       revalidateIfStale: true,
       refreshWhenHidden: true,
       refreshWhenOffline: true,
-      refreshInterval: 1000 * 60 * 30 // 30 minutes
+      refreshInterval: 1000 * 60 * 10 // 10 minutes
     }
   )
 
