c26bc0f2a2e8f074dfa3997a2c75026c831d3710
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-02-08 10:02:31 +0100

    updates

--- components/marketing/header.tsx
+++ components/marketing/header.tsx
@@ -8,7 +8,7 @@ import { ThemeToggle } from "@/components/theme/toggle"
 
 export function MarketingHeader() {
   return (
-    <header className="sticky top-0 z-50 flex h-16 w-full shrink-0 items-center justify-between border-b bg-muted px-4 backdrop-blur-xl">
+    <header className="sticky top-0 z-50 flex h-16 w-full shrink-0 items-center justify-between bg-muted px-4 backdrop-blur-xl">
       <div className="flex items-center">
         <Link href="/">
           <Image
