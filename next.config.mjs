b6d9e4442cdf4881a147e4276823dc9e32d72516
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-02-15 05:48:05 +0100

    updates

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,14 @@ const nextConfig = {
       fullUrl: true
     }
   },
+  headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders
+      }
+    ]
+  },
   webpack: config => {
     config.resolve.alias.canvas = false
     return config
@@ -31,3 +39,45 @@ const millionConfig = {
 }
 
 export default million.next(nextConfig, millionConfig)
+
+const ContentSecurityPolicy = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' cdn.vercel-insights.com va.vercel-scripts.com;
+    style-src 'self' 'unsafe-inline';
+    img-src * blob: data:;
+    media-src 'none';
+    connect-src *;
+    font-src 'self' data:;
+    frame-src 'self' *.codesandbox.io;
+`
+
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\n/g, "")
+  },
+  {
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin"
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN"
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff"
+  },
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on"
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=31536000; includeSubDomains; preload"
+  },
+  {
+    key: "Permissions-Policy",
+    value: "camera=(), microphone=(), geolocation=()"
+  }
+]
