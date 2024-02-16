f108de9498957adbe6a1ec206b0ed6bffc88f7d8
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-02-16 08:12:28 +0100

    updates

--- lib/supabase/admin/blogs.ts
+++ lib/supabase/admin/blogs.ts
@@ -1,27 +1,19 @@
-import { unstable_cache as cache } from "next/cache"
-
 import supabaseAdmin from "."
 
-export const getBlogsBySlugAdmin = cache(
-  async (slug: string) => {
-    const { data, error } = await supabaseAdmin
-      .from("blogs")
-      .select("*")
-      .eq("slug", slug)
-      .limit(1)
-      .maybeSingle()
+export const getBlogsBySlugAdmin = async (slug: string) => {
+  const { data, error } = await supabaseAdmin
+    .from("blogs")
+    .select("*")
+    .eq("slug", slug)
+    .limit(1)
+    .maybeSingle()
 
-    if (error) {
-      throw error
-    }
-
-    return data
-  },
-  ["getBlogsBySlugAdmin"],
-  {
-    revalidate: 3600
+  if (error) {
+    throw error
   }
-)
+
+  return data
+}
 
 export const getBlogsAdmin = async () => {
   const { data, error } = await supabaseAdmin
