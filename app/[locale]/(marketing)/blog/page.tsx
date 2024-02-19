70f32e8fd48fab836ced8cfca1a32eacca7cf8fb
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-02-19 13:18:15 +0100

    updates

--- app/[locale]/(marketing)/blog/page.tsx
+++ app/[locale]/(marketing)/blog/page.tsx
@@ -53,7 +53,7 @@ export default async function BlogPage() {
           </div>
 
           <div className="pb-12">
-            <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4">
+            <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
               {blogs.map(blog => {
                 const cover = getBlogsCoverPublicUrlAdmin(blog.id, blog.cover)
 
