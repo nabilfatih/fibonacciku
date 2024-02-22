c978070028779629b0855e627bf80038954fd734
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-02-22 14:10:38 +0100

    updates

--- components/marketing/transition.tsx
+++ components/marketing/transition.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { motion } from "framer-motion"
+import { AnimatePresence, motion } from "framer-motion"
 
 type Props = {
   children: React.ReactNode
@@ -9,18 +9,20 @@ type Props = {
 
 export default function MarketingTransition({ children, className }: Props) {
   return (
-    <motion.main
-      // transition effect for page transitions
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{
-        duration: 0.5,
-        ease: "easeInOut"
-      }}
-      className={className}
-    >
-      {children}
-    </motion.main>
+    <AnimatePresence mode="wait">
+      <motion.main
+        // transition effect for page transitions
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{
+          duration: 0.5,
+          ease: "easeInOut"
+        }}
+        className={className}
+      >
+        {children}
+      </motion.main>
+    </AnimatePresence>
   )
 }
