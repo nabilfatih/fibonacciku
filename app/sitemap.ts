3b76d7cc555c5621332ecec4fa02e2df18b99c59
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-02-23 11:21:34 +0100

    updates

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -42,6 +42,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       return {
         url: `${baseUrl}${locale}/blog/${blog.slug}`,
         lastModified: new Date(blog.updated_at).toISOString().split("T")[0],
+        changeFrequency: "weekly",
         priority: 1
       }
     })
