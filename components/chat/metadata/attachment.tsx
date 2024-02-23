174e60597f20f88d6df465a59bf4a964e476d956
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-02-23 12:27:47 +0100

    updates

--- components/chat/metadata/attachment.tsx
+++ components/chat/metadata/attachment.tsx
@@ -1,25 +1,10 @@
 import React, { memo } from "react"
 import Image from "next/image"
 import Link from "next/link"
-import useSWRImmutable from "swr/immutable"
 
-import type { Attachment, UserDetails } from "@/types/types"
+import type { Attachment } from "@/types/types"
 import { useCurrentUser } from "@/lib/context/use-current-user"
-import { getChatAttachmentSignedUrl } from "@/lib/supabase/client/chat"
-
-async function fetchChatAttachment([userDetails, metadata]: [
-  UserDetails,
-  Attachment[]
-]) {
-  const userId = userDetails.id
-  if (!userId) return []
-  const urls = await Promise.all(
-    metadata.map(item =>
-      getChatAttachmentSignedUrl(userId, item.chat_id, item.file_id)
-    )
-  )
-  return urls
-}
+import { getChatAttachmentPublicUrl } from "@/lib/supabase/client/chat"
 
 type Props = {
   metadata: Attachment[]
@@ -27,19 +12,17 @@ type Props = {
 
 function ChatMetadataAttachment({ metadata }: Props) {
   const { userDetails } = useCurrentUser()
-  const { data: imageUrls, isLoading } = useSWRImmutable(
-    [userDetails, metadata],
-    fetchChatAttachment,
-    {
-      refreshInterval: 1000 * 60 * 60 * 24 // 24 hours
-    }
-  )
 
-  if (isLoading || !imageUrls) return <Loading />
+  if (!userDetails) return null
 
   return (
     <div className="grid grid-cols-2 gap-4">
-      {imageUrls.map((url, index) => {
+      {metadata.map((attachment, index) => {
+        const url = getChatAttachmentPublicUrl(
+          userDetails.id,
+          attachment.chat_id,
+          attachment.file_id
+        )
         return (
           <Link key={url} href={url} target="_blank">
             <Image
@@ -59,14 +42,6 @@ function ChatMetadataAttachment({ metadata }: Props) {
           </Link>
         )
       })}
-    </div>
-  )
-}
-
-function Loading() {
-  return (
-    <div className="grid grid-cols-2 gap-4">
-      <div className="m-0 block h-64 w-64 rounded-xl border bg-muted/90 object-cover" />
     </div>
   )
 }
