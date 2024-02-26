6bf65f4beb424c04199372861356e98ac78a3c78
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-02-26 16:29:00 +0100

    updates

--- app/robots.ts
+++ app/robots.ts
@@ -2,10 +2,11 @@ import type { MetadataRoute } from "next"
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/" // Allow everything
-    },
+    rules: [
+      {
+        userAgent: "*"
+      }
+    ],
     sitemap: "https://www.fibonacciku.com/sitemap.xml",
     host: "https://www.fibonacciku.com"
   }
