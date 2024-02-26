a98816b6d039fce3624cfa4d90583b0a73ca7495
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-02-26 21:06:24 +0100

    updates

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -119,11 +119,9 @@ export async function generateMetadata(): Promise<Metadata> {
     robots: {
       index: true,
       follow: true,
-      nocache: false,
       googleBot: {
         index: true,
         follow: true,
-        noimageindex: false,
         "max-video-preview": -1,
         "max-image-preview": "large",
         "max-snippet": -1
