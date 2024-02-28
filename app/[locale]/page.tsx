8ef18673a100ec9da5b16787260c324cdb5d2b77
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-02-28 21:55:35 +0100

    updates

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,8 +1,11 @@
 import { cookies } from "next/headers"
 import { redirect } from "next/navigation"
 
+import { CurrentUserContextProvider } from "@/lib/context/use-current-user"
 import { createClientServer } from "@/lib/supabase/server"
 
+import MarketingFooter from "@/components/marketing/footer"
+import MarketingHeader from "@/components/marketing/header"
 import MarketingHome from "@/components/marketing/home"
 
 export default async function Home() {
@@ -17,5 +20,13 @@ export default async function Home() {
     redirect("/chat/assistant")
   }
 
-  return <MarketingHome />
+  return (
+    <CurrentUserContextProvider session={session}>
+      <MarketingHeader />
+      <main className="flex flex-1 flex-col overflow-hidden">
+        <MarketingHome />
+      </main>
+      <MarketingFooter />
+    </CurrentUserContextProvider>
+  )
 }
