caab2c5b1c9ceae82fc02f96aeeb3ce4d65cfe79
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-02-28 09:24:38 +0100

    updates

--- components/marketing/transition.tsx
+++ components/marketing/transition.tsx
@@ -11,12 +11,12 @@ export default function MarketingTransition({ children, className }: Props) {
   return (
     <AnimatePresence mode="wait">
       <motion.main
-        // transition effect for page transitions
+        // transition effect for page transitions, opacity
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
         transition={{
-          duration: 0.5,
+          duration: 0.75,
           ease: "easeInOut"
         }}
         className={className}
