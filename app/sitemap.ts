5d9af3caab30fc6aa227bf2609ee7b1f920c9efb
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-03-01 16:39:53 +0100

    updates

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -35,7 +35,6 @@ const pathnames = [
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = "https://www.fibonacciku.com"
-
   const blogSlugs = await getBlogsSlugAdmin()
 
   const blogsPaths: MetadataRoute.Sitemap = locales.flatMap(locale => {
