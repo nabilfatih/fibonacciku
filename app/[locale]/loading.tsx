46b7a6572f7aaa5367a4ba3a791e5e63034298cb
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-03-08 17:52:48 +0100

    updates

--- app/[locale]/loading.tsx
+++ app/[locale]/loading.tsx
@@ -2,14 +2,8 @@ import { IconSpinner } from "@/components/ui/icons"
 
 export default function Loading() {
   return (
-    <main className="flex h-screen flex-1 flex-col overflow-hidden">
-      <div className="relative">
-        <div className="absolute inset-0 flex items-center justify-center">
-          <div className="flex flex-col items-center">
-            <IconSpinner className="h-12 w-12 animate-spin" />
-          </div>
-        </div>
-      </div>
-    </main>
+    <div className="flex h-screen items-center justify-center">
+      <IconSpinner className="h-8 w-8 animate-spin" />
+    </div>
   )
 }
