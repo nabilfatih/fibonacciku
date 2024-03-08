fe96a48e7f1611fd6860160fcdfde7b90f55860a
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-03-08 18:31:06 +0100

    updates

--- app/[locale]/loading.tsx
+++ app/[locale]/loading.tsx
@@ -2,7 +2,7 @@ import { IconSpinner } from "@/components/ui/icons"
 
 export default function Loading() {
   return (
-    <div className="flex h-screen items-center justify-center">
+    <div className="flex h-[100dvh] items-center justify-center">
       <IconSpinner className="h-8 w-8 animate-spin" />
     </div>
   )
