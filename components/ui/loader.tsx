2b798e49aeececf6b88c7fad85f4054cbdc055a3
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-03-11 14:56:25 +0100

    updates

--- components/ui/loader.tsx
+++ components/ui/loader.tsx
@@ -1,29 +1,19 @@
 "use client"
 
-import { memo, useMemo } from "react"
+import { memo } from "react"
 import { AppProgressBar as ProgressBar } from "next-nprogress-bar"
-import { useTheme } from "next-themes"
-
-import { darkThemes } from "@/lib/data/themes"
 
 type Props = {
   children: React.ReactNode
 }
 
 function Loader({ children }: Props) {
-  const { resolvedTheme } = useTheme()
-
-  const color = useMemo(() => {
-    if (!resolvedTheme) return "#18181b"
-    return darkThemes.includes(resolvedTheme) ? "#f4f4f5" : "#18181b"
-  }, [resolvedTheme])
-
   return (
     <>
       {children}
       <ProgressBar
         height="3px"
-        color={color}
+        color="var(--foreground)"
         options={{ showSpinner: false }}
       />
     </>
