c89eccb95919730eba15ff8d3d7f452f10c029d4
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2024-03-18 09:04:46 +0100

    updates

--- components/chat/empty-screen.tsx
+++ components/chat/empty-screen.tsx
@@ -32,12 +32,12 @@ export default function EmptyScreen({ type }: Props) {
   if (!userDetails) return null
 
   // only get the first name
-  const name = `, ${userDetails.full_name?.split(" ")[0]}` || ""
+  const name = userDetails.full_name?.split(" ")[0] || ""
 
   const greeting = `${t(`good-${defineGreeting()}`)}`
 
   // Split the text into individual characters
-  const textArray = `${greeting}${name}`.split("").concat(" 👋")
+  const textArray = `${greeting} ${name}`.split("").concat(" 👋")
 
   return (
     <div className="mx-auto max-w-2xl px-4">
